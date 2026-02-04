7476f720e7ff1fe2b61d6dd0790a1fa99d61986d
tilt-rgb <lesma_chan@pm.me>
2026-02-04 14:08:07 -0300

    sm7325-common: Include Sony Dolby Atmos * Using dolby from: https://github.com/swiitch-OFF-Lab/hardware_dolby [1]: sm8350-common: Add dolby volume listener effect [2]: sm8350-common: audio: Bring all stream effects
    Co-Authored-By: HELLBOY017 <abhaygill017@gmail.com>
    Signed-off-by: swiitchOFF <120115258+swiitchOFF@users.noreply.github.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -157,7 +157,7 @@ BOARD_SUPPORTS_SOUND_TRIGGER := true
 
 # HIDL manifests
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/configs/manifest_lahaina.xml
-DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
+DEVICE_MATRIX_FILE += hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     $(COMMON_PATH)/configs/framework_compatibility_matrix.xml \
     hardware/samsung/vintf/samsung_framework_compatibility_matrix.xml \
